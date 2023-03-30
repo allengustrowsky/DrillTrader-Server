30b0bca01037f4551dccd5ea7da3481b2b21db59
Allen Gustrowsky <allen@may21.us>
2023-03-30 15:07:20 -0400

    Project setup

---
+++ src/asset/asset.service.ts
@@ -0,0 +1,30 @@
+import { EntityManager } from '@mikro-orm/mysql';
+import { Injectable } from '@nestjs/common';
+import { CreateAssetDto } from './dto/create-asset.dto';
+import { UpdateAssetDto } from './dto/update-asset.dto';
+
+@Injectable()
+export class AssetService {
+  constructor(private readonly em: EntityManager) {}
+
+  create(createAssetDto: CreateAssetDto) {
+    return 'This action adds a new asset';
+  }
+
+  findAll() {
+    // return this.em.find(id)
+    return `This action returns all asset`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} asset`;
+  }
+
+  update(id: number, updateAssetDto: UpdateAssetDto) {
+    return `This action updates a #${id} asset`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} asset`;
+  }
+}
---
+++ src/asset/entities/asset.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+@Entity()
+export class Asset {
+    @PrimaryKey({
+        autoincrement: true,
+    })
+    id: number
+
+    @Property({
+        length: 64,
+    })
+    name: string
+
+    // TODO: make foreign key
+    @Property()
+    asset_type_id: number
+
+    @Property({
+        length: 16,
+    })
+    ticker_symbol: string
+}

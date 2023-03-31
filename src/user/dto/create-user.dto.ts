c1f033dae26b52de364185768c9862119d2bcf75
Allen Gustrowsky <allen@may21.us>
2023-03-30 21:02:47 -0400

    User DTO

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,23 @@
-export class CreateUserDto {}
+import { IsBoolean, IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
+
+export class CreateUserDto {
+    @IsNotEmpty()
+    @Length(1, 32)
+    @IsString()
+    first_name: string;
+
+    @IsNotEmpty()
+    @Length(1, 32)
+    @IsString()
+    last_name: string;
+
+    @IsNotEmpty()
+    @Length(1, 32)
+    @IsEmail()
+    email_address: string;
+
+    @IsNotEmpty()
+    @Length(1, 128)
+    password_hash: string;
+}

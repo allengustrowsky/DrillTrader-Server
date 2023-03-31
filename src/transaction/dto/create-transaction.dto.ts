c0e62f9d919e64d71d439559d977a491d6250835
Allen Gustrowsky <allen@may21.us>
2023-03-30 21:29:40 -0400

    Transaction DTO

--- src/transaction/dto/create-transaction.dto.ts
+++ src/transaction/dto/create-transaction.dto.ts
@@ -1,1 +1,28 @@
-export class CreateTransactionDto {}
+import { IsBoolean, IsInt, IsNotEmpty, IsNumber, IsPositive } from "class-validator";
+
+
+export class CreateTransactionDto {
+    @IsNotEmpty()
+    @IsInt()
+    @IsPositive()
+    portfolio_id: number;
+
+    @IsNotEmpty()
+    @IsInt()
+    @IsPositive()
+    asset_id: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    units: number;
+
+    @IsNotEmpty()
+    @IsNumber()
+    @IsPositive()
+    price: number;
+
+    @IsNotEmpty()
+    @IsBoolean()
+    is_buy: boolean;
+}

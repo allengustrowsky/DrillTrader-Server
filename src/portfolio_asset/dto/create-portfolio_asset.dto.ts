25c0a5d5bc8967d13cf26bf78caa26c81133a3f0
Allen Gustrowsky <allen@may21.us>
2023-04-04 16:14:30 -0400

    Portfolio_asset create DTO

--- src/portfolio_asset/dto/create-portfolio_asset.dto.ts
+++ src/portfolio_asset/dto/create-portfolio_asset.dto.ts
@@ -1,1 +1,14 @@
-export class CreatePortfolioAssetDto {}
+import { IsInt, IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreatePortfolioAssetDto {
+    @IsNotEmpty()
+    @IsInt()
+    portfolio_id: number;
+    
+    @IsNotEmpty()
+    @IsInt()
+    asset_id: number;
+    
+    @IsNumber()
+    units: number;
+}

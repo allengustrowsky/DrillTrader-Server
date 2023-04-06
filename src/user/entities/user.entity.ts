439262d03b477056afd243673ad278215e32f470
Allen Gustrowsky <allen@may21.us>
2023-04-05 21:52:31 -0400

    Fixed length of User auto-generated Apikey

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,8 +1,16 @@
 import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 import { randomBytes } from 'crypto';
+import { create } from 'domain';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Entity()
 export class User {
+    constructor(createUserDto: CreateUserDto) {
+        this.first_name = createUserDto.first_name
+        this.last_name = createUserDto.last_name
+        this.email_address = createUserDto.email_address
+    }
+
     @PrimaryKey({
         autoincrement: true,
     })
@@ -26,7 +34,8 @@ export class User {
     @Property({
         length: 128,
     })
-    apiKey = randomBytes(128).toString('hex');
+    // credit: phoenix2010 from https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
+    apiKey = randomBytes(64).toString('hex');
 
     @Property()
     is_admin?: boolean = false;

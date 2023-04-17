51832c6e59e20d4d6847583e37768f9bf2f0b2df
Allen Gustrowsky <allen@may21.us>
2023-04-16 20:20:22 -0400

    Modified user updatedDto

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,42 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import {
+    IsBoolean,
+    IsEmail,
+    IsInt,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Length,
+} from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+// export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+    @ApiPropertyOptional({
+        type: String,
+        description: 'This is an optional property',
+    })
+    @Length(1, 32)
+    @IsString()
+    @IsOptional()
+    first_name: string;
+
+    @ApiPropertyOptional({
+        type: String,
+        description: 'This is an optional property',
+    })
+    @Length(1, 32)
+    @IsString()
+    @IsOptional()
+    last_name: string;
+
+    @ApiPropertyOptional({
+        type: String,
+        description: 'This is an optional property',
+    })
+    @Length(1, 32)
+    @IsEmail()
+    @IsOptional()
+    email_address: string;
+}

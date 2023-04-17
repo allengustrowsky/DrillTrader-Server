8e381029163ce71ce11f9dad5fd532826727c368
Allen Gustrowsky <allen@may21.us>
2023-04-17 06:22:26 -0400

    Created sample data endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
     getHello(): string {
         return this.appService.getHello();
     }
+
+    @Get('/setup')
+    setup() {
+        return this.appService.setup();
+    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user/entities/user.entity';
+import { EntityManager } from '@mikro-orm/mysql';
+import { Portfolio } from './portfolio/entities/portfolio.entity';
+import { Asset } from './asset/entities/asset.entity';
+import { AssetType } from './asset_type/entities/asset_type.entity';
 
 @Injectable()
 export class AppService {
+    constructor(private readonly em: EntityManager) {}
+
     getHello(): string {
         return 'Hello World!';
+    }
+
+    async setup(): Promise<string> {
+        const user1 = new User({
+            first_name: "Allen",
+            last_name: "Gustrowsky",
+            email_address: "allen@may21.us"
+        })
+        await this.em.persistAndFlush(user1);
+        const p1 = new Portfolio()
+        p1.user = user1;
+        await this.em.persistAndFlush(p1)
+
+        const user2 = new User({
+            first_name: "Brandon",
+            last_name: "Gustrowsky",
+            email_address: "brandon@may21.us"
+        })
+        await this.em.persistAndFlush(user2);
+        const p2 = new Portfolio()
+        p2.user = user2;
+        await this.em.persistAndFlush(p2)
+
+        const a1 = new AssetType({
+            name: "Stock"
+        })
+        await this.em.persistAndFlush(a1);
+
+        return 'Success'
     }
 }

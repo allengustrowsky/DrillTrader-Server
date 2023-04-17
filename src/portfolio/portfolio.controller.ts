f8a588e186cf320b0338f7409f147b80e6f60613
Allen Gustrowsky <allen@may21.us>
2023-04-17 18:53:56 -0400

    Portfolio routes + documentation

--- src/portfolio/portfolio.controller.ts
+++ src/portfolio/portfolio.controller.ts
@@ -10,37 +10,50 @@ import {
 import { PortfolioService } from './portfolio.service';
 import { CreatePortfolioDto } from './dto/create-portfolio.dto';
 import { UpdatePortfolioDto } from './dto/update-portfolio.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+/**
+ * This route should not be used often. With the way portfolios are 
+ * designed to work, there should rarely be a request made to fetch 
+ * or delete portfolios, if ever. They are auto-generated when a new 
+ * user is made and should never need to be modified after that. Deleting 
+ * a user will automatically delete their portfolio. In light of all of 
+ * this, only GET and DELETE routes have been implemented.
+ */
 @ApiTags('Portfolio')
 @Controller('portfolio')
 export class PortfolioController {
     constructor(private readonly portfolioService: PortfolioService) {}
 
-    @Post()
-    create(@Body() createPortfolioDto: CreatePortfolioDto) {
-        return this.portfolioService.create(createPortfolioDto);
-    }
+    // @Post()
+    // create(@Body() createPortfolioDto: CreatePortfolioDto) {
+    //     return this.portfolioService.create(createPortfolioDto);
+    // }
 
     @Get()
+    @ApiOkResponse({ description: 'Successfully returned resources.' })
     findAll() {
         return this.portfolioService.findAll();
     }
 
     @Get(':id')
+    @ApiOkResponse({ description: 'Successfully returned resource.' })
+    @ApiNotFoundResponse({ description: 'Portfolio with this id not found.' })
     findOne(@Param('id') id: string) {
         return this.portfolioService.findOne(+id);
     }
 
-    @Patch(':id')
-    update(
-        @Param('id') id: string,
-        @Body() updatePortfolioDto: UpdatePortfolioDto,
-    ) {
-        return this.portfolioService.update(+id, updatePortfolioDto);
-    }
+    // @Patch(':id')
+    // update(
+    //     @Param('id') id: string,
+    //     @Body() updatePortfolioDto: UpdatePortfolioDto,
+    // ) {
+    //     return this.portfolioService.update(+id, updatePortfolioDto);
+    // }
 
     @Delete(':id')
+    @ApiOkResponse({ description: 'Successfully deleted resource.' })
+    @ApiNotFoundResponse({ description: 'Portfolio with this id not found.' })
     remove(@Param('id') id: string) {
         return this.portfolioService.remove(+id);
     }
--- src/portfolio/portfolio.service.ts
+++ src/portfolio/portfolio.service.ts
@@ -1,29 +1,42 @@
 import { EntityManager } from '@mikro-orm/mysql';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreatePortfolioDto } from './dto/create-portfolio.dto';
 import { UpdatePortfolioDto } from './dto/update-portfolio.dto';
+import { Portfolio } from './entities/portfolio.entity';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class PortfolioService {
     constructor(private readonly em: EntityManager) {}
 
-    create(createPortfolioDto: CreatePortfolioDto) {
-        return 'This action adds a new portfolio';
+    // create(createPortfolioDto: CreatePortfolioDto) {
+    //     return 'This action adds a new portfolio';
+    // }
+
+    async findAll() {
+        const portfolios = await this.em.find(Portfolio, {});
+        return portfolios;
     }
 
-    findAll() {
-        return `This action returns all portfolio`;
+    async findOne(id: number) {
+        const portfolio = await this.em.findOne(Portfolio, id);
+        if (!portfolio) {
+            throw new NotFoundException(`Portfolio with id ${id} not found.`)
+        }
+        return portfolio;
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} portfolio`;
-    }
+    // update(id: number, updatePortfolioDto: UpdatePortfolioDto) {
+    //     return `This action updates a #${id} portfolio`;
+    // }
 
-    update(id: number, updatePortfolioDto: UpdatePortfolioDto) {
-        return `This action updates a #${id} portfolio`;
-    }
-
-    remove(id: number) {
-        return `This action removes a #${id} portfolio`;
+    async remove(id: number) {
+        const portfolio = await this.em.findOne(Portfolio, id);
+        if (portfolio) {
+            await this.em.remove(portfolio).flush()
+            return portfolio
+        } else {
+            throw new NotFoundException(`Portfolio with id ${id} not found`);
+        }
     }
 }

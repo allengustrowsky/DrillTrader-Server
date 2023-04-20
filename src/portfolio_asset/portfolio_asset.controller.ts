a629b0ac88a274f0465fcd7bed435e926b36f012
Allen Gustrowsky <allen@may21.us>
2023-04-20 10:14:20 -0400

    Most of portfolio_asset routes done

--- src/portfolio_asset/portfolio_asset.controller.ts
+++ src/portfolio_asset/portfolio_asset.controller.ts
@@ -6,11 +6,15 @@ import {
     Patch,
     Param,
     Delete,
+    UseGuards,
+    Req,
 } from '@nestjs/common';
 import { PortfolioAssetService } from './portfolio_asset.service';
 import { CreatePortfolioAssetDto } from './dto/create-portfolio_asset.dto';
 import { UpdatePortfolioAssetDto } from './dto/update-portfolio_asset.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiConflictResponse, ApiMethodNotAllowedResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiAuthGuard } from 'src/auth/auth.guard';
+import { IsAdminGuard } from 'src/auth/admin.guard';
 
 @ApiTags('Portfolio-Asset')
 @Controller('portfolio-asset')
@@ -19,21 +23,45 @@ export class PortfolioAssetController {
         private readonly portfolioAssetService: PortfolioAssetService,
     ) {}
 
+    @UseGuards(IsAdminGuard)
+    @UseGuards(ApiAuthGuard)
     @Post()
+    @ApiOkResponse({ description: 'Successfully created resources.' })
+    @ApiBadRequestResponse({ description: 'Unites must not be less than zero.' })
+    @ApiNotFoundResponse({ description: 'Portfolio with given id not found.' })
+    @ApiNotFoundResponse({ description: 'Asset with given id not found.' })
+    @ApiConflictResponse({ description: 'Portfolio asset of this type for this user\'s portfolio already exists.'})
     create(@Body() createPortfolioAssetDto: CreatePortfolioAssetDto) {
         return this.portfolioAssetService.create(createPortfolioAssetDto);
     }
 
+    @UseGuards(IsAdminGuard)
+    @UseGuards(ApiAuthGuard)
     @Get()
+    @ApiOkResponse({ description: 'Successfully returned resources.' })
     findAll() {
         return this.portfolioAssetService.findAll();
     }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.portfolioAssetService.findOne(+id);
+    @UseGuards(ApiAuthGuard)
+    @Get('/user/:id')
+    @ApiOkResponse({ description: 'Successfully returned resources.' })
+    @ApiMethodNotAllowedResponse({ description: 'You are not allowed to access this user\'s portfolio assets.' })
+    findAllUser(@Param('id') id: string, @Req() request: Request) {
+        return this.portfolioAssetService.findAllUser(+id, request);
     }
 
+    @UseGuards(ApiAuthGuard)
+    @Get(':id')
+    @ApiOkResponse({ description: 'Successfully returned resource.' })
+    @ApiNotFoundResponse({ description: 'Portfolio asset with this id not found.' })
+    @ApiMethodNotAllowedResponse({ description: 'You are not allowed to access this user\'s portfolio assets.' })
+    findOne(@Param('id') id: string, @Req() request: Request) {
+        return this.portfolioAssetService.findOne(+id, request);
+    }
+
+    @UseGuards(IsAdminGuard)
+    @UseGuards(ApiAuthGuard)
     @Patch(':id')
     update(
         @Param('id') id: string,
@@ -42,7 +70,11 @@ export class PortfolioAssetController {
         return this.portfolioAssetService.update(+id, updatePortfolioAssetDto);
     }
 
+    @UseGuards(IsAdminGuard)
+    @UseGuards(ApiAuthGuard)
     @Delete(':id')
+    @ApiOkResponse({ description: 'Successfully deleted resource.' })
+    @ApiNotFoundResponse({ description: 'Portfolio asset with this id not found.' })
     remove(@Param('id') id: string) {
         return this.portfolioAssetService.remove(+id);
     }
--- src/portfolio_asset/portfolio_asset.service.ts
+++ src/portfolio_asset/portfolio_asset.service.ts
@@ -1,32 +1,90 @@
 import { EntityManager } from '@mikro-orm/mysql';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePortfolioAssetDto } from './dto/create-portfolio_asset.dto';
 import { UpdatePortfolioAssetDto } from './dto/update-portfolio_asset.dto';
 import { PortfolioAsset } from './entities/portfolio_asset.entity';
+import { Portfolio } from 'src/portfolio/entities/portfolio.entity';
+import { Asset } from 'src/asset/entities/asset.entity';
 
 @Injectable()
 export class PortfolioAssetService {
     constructor(private readonly em: EntityManager) {}
 
     async create(createPortfolioAssetDto: CreatePortfolioAssetDto) {
+        if (createPortfolioAssetDto.units < 0) {
+            throw new HttpException(`Units must not be less than zero.`, HttpStatus.BAD_REQUEST);
+        }
+
         const portfolio_asset = new PortfolioAsset(createPortfolioAssetDto)
+
+        // set portfolio
+        const portfolio = await this.em.findOne(Portfolio, createPortfolioAssetDto.portfolio_id);
+        if (!portfolio) {
+            throw new HttpException(`Portfolio with id ${createPortfolioAssetDto.portfolio_id} not found.`, HttpStatus.NOT_FOUND);
+        } else {
+            portfolio_asset.portfolio = portfolio;
+        }
+
+        // set asset
+        const asset = await this.em.findOne(Asset, createPortfolioAssetDto.asset_id);
+        if (!asset) {
+            throw new HttpException(`Asset with id ${createPortfolioAssetDto.asset_id} not found.`, HttpStatus.NOT_FOUND);
+        } else {
+            portfolio_asset.asset = asset;
+        }
+
+        // portfolio_id and asset_id together must be unique
+        const duplicate_pa = await this.em.find(PortfolioAsset, {asset: asset, portfolio: portfolio})
+        if (duplicate_pa.length > 0) {
+            throw new HttpException(`Portfolio asset of this type for this user's portfolio already exists.`, HttpStatus.CONFLICT);
+        }
+
         await this.em.persistAndFlush(portfolio_asset)
         return portfolio_asset
     }
 
-    findAll() {
-        return `This action returns all portfolioAsset`;
+    async findAllUser(id: number, request: Request) {
+        // make sure user is allowed to access this data (owner or admin)
+        const isAuthorized = id === (request as any).user.id || (request as any).user.is_admin
+        if (!isAuthorized) {
+            throw new HttpException('You are not allowed to access this user\'s portfolio assets.', HttpStatus.FORBIDDEN);
+        }
+
+        const portfolio_assets = this.em.find(PortfolioAsset, { portfolio: id })
+        return portfolio_assets
     }
 
-    findOne(id: number) {
-        return `This action returns a #${id} portfolioAsset`;
+    async findAll() {
+        const portfolio_assets = await this.em.find(PortfolioAsset, {});
+        return portfolio_assets;
+    }
+
+    async findOne(id: number, request: Request) {
+        // make sure user is allowed to access this data (owner or admin)
+        const portfolioAsset = await this.em.findOne(PortfolioAsset, id)
+        if (!portfolioAsset) {
+            throw new HttpException('Portfolio asset not found.', HttpStatus.NOT_FOUND);
+        }
+
+        const isAuthorized = portfolioAsset.portfolio.id === (request as any).user.id || (request as any).user.is_admin
+        if (!isAuthorized) {
+            throw new HttpException('You are not allowed to access this portfolio asset.', HttpStatus.FORBIDDEN);
+        }
+
+        return portfolioAsset;
     }
 
     update(id: number, updatePortfolioAssetDto: UpdatePortfolioAssetDto) {
         return `This action updates a #${id} portfolioAsset`;
     }
 
-    remove(id: number) {
-        return `This action removes a #${id} portfolioAsset`;
+    async remove(id: number) {
+        const portfolio_asset = await this.em.findOne(PortfolioAsset, id);
+        if (!portfolio_asset) {
+            throw new HttpException(`Portfolio asset with id ${id} not found.`, HttpStatus.NOT_FOUND);
+        }
+        
+        await this.em.removeAndFlush(portfolio_asset)
+        return portfolio_asset;
     }
 }

149deab10b87d0dac53154b8caebe6d3ee7fa93d
Allen Gustrowsky <allen@may21.us>
2023-04-21 17:16:07 -0400

    Current progress on cron job (pausing)

--- src/cron/portfolio_value_logger_cron.service.ts
+++ src/cron/portfolio_value_logger_cron.service.ts
@@ -1,13 +1,25 @@
+import { EntityManager } from "@mikro-orm/mysql";
 import { Injectable } from "@nestjs/common";
 import { Cron, CronExpression } from "@nestjs/schedule";
+import { Portfolio } from "src/portfolio/entities/portfolio.entity";
 
 // adapted from nest's article on task scheduling: https://docs.nestjs.com/techniques/task-scheduling
 @Injectable()
 export class PortfolioValueLogger {
+    constructor(private readonly em: EntityManager) {}
 
     @Cron(CronExpression.EVERY_10_SECONDS)
-    logPortfolioValue() {
+    async logPortfolioValue() {
+        const portfolios = await this.em.find(Portfolio, {})
         console.log('cron job running! eyyyyyy')
-        // this.logger.debug('cron job called')
+        
+        // for each portfolio
+            // get all portfolio assets in that portfolio
+            // for each portfolio asset
+                // sum = p_asset.units * p_asset.
+
+        // cash portfolio asset handle differently
+        // if this cannot be handled with web sockets, then put use limit is reached by finnhub api rate limits bc have to 
+        // time out until rate limit refills
     }
 }
